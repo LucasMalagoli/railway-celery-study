2e2a08278a571f194590a1fceb036528c0d64f2b
LucasMalagoli <mpslucas14@gmail.com>
2025-08-28 21:05:30 -0300

    test debug script

---
+++ app/run_worker.py
@@ -0,0 +1,7 @@
+import os
+from tasks import celery
+
+if __name__ == "__main__":
+    print("Starting worker...")
+    print("REDIS_URL =", os.getenv("REDIS_URL"))
+    celery.worker_main(["worker", "--loglevel=info"])
